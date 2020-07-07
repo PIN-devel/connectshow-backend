21eef6a5471b5b701bca41a403ca7f4c6a1a6cca
soomi <tnal1632@naver.com>
2020-07-07 15:38:57 +0900

    community serializer&url&views update

--- community/urls.py
+++ community/urls.py
@@ -1,6 +1,10 @@
-
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-   
+    path('', views.list_or_create),
+    path('<int:article_id>/', views.detail_or_update_or_delete),
+    path('<int:article_id>/comments/', views.comment_list_or_create),
+    path('<int:article_id>/comments/<int:comment_id>/', views.comment_update_or_delete),
 ]
--- community/views.py
+++ community/views.py
@@ -1,3 +1,106 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Article, Comment
+from .serializers import ArticleSerializer, ArticleListSerializer, CommentSerializer,CommentListSerializer
+from accounts.models import Club
+
+# Articles
+@api_view(['GET','POST'])
+def list_or_create(request):
+  if request.method == 'GET':
+    p = request.GET.get('page', 1)
+    articles = Paginator(Article.objects.order_by('-id'), 10)
+    serializer = ArticleListSerializer(articles.page(p), many=True)
+    return Response({"status":"OK", "data":serializer.data})
+  else:
+    if request.user.is_authenticated:
+      club_id = request.data.get('club_id')
+      club = get_object_or_404(Club, id=club_id)
+      if request.user == club.master:
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+          serializer.save(user=request.user, club=club)
+          return Response({"status":"OK", "data":serializer.data})
+      else:
+        return Response({"status":"FAIL", "error_msg":"클럽 마스터만 글을 작성할 수 있습니다."}, status=status.HTTP_403_FORBIDDEN)
+    else:
+      return Response({"status":"FAIL", "error_msg":"로그인이 필요한 서비스입니다."}, status=status.HTTP_401_UNAUTHORIZED)
+      
+
+@api_view(['GET','PUT','DELETE'])
+def detail_or_update_or_delete(request, article_id):
+  article = get_object_or_404(Article, id=article_id)
+  if request.method=='GET':
+    serializer = ArticleListSerializer(article)
+    return Response({"status":"OK", "data":serializer.data})
+
+  if request.user.is_authenticated:
+    if request.method == 'DELETE':
+      if request.user == article.user:
+        article.delete()
+        return Response({"status":"OK"})
+      else:
+        return Response({"status":"FAIL", "error_msg":"클럽 마스터만 삭제할 수 있습니다."}, status=status.HTTP_403_FORBIDDEN)
+
+    else:
+      if request.user == article.user:
+        club_id = request.get.GET('club_id')
+        club = Club.objects.filter(id=club_id)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+          serializer.save(user=request.user, club=club)
+        # article.title = request.data.get('title')
+        # article.content = request.data.get('content')
+        # article.save()
+        # serializer = ArticleSerializer(article)
+        return Response({"status":"OK", "data":serializer.data})
+      else:
+        return Response({"status":"FAIL","error_msg":"클럽 마스터만 수정할 수 있습니다."},status=status.HTTP_403_FORBIDDEN)
+  else:
+    return Response({"status":"FAIL", "error_msg":"로그인이 필요한 서비스입니다."}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(['GET','POST'])
+def comment_list_or_create(request,article_id):
+  if request.method == 'GET':
+    comments = Comment.objects.filter(article_id=article_id)
+    serializer = CommentListSerializer(comments, many=True)
+    return Response({"status":"OK", "data":serializer.data})
+  else:
+    if request.user.is_authenticated:
+      article = get_object_or_404(Article, id=article_id)
+      serializer = CommentSerializer(data=request.data)
+      if serializer.is_valid(raise_exception=True):
+        serializer.save(user=request.user, article=article)
+        return Response({"status":"OK", "data":serializer.data})
+    else:
+      return Response({"status":"FAIL", "error_msg":"로그인이 필요한 서비스입니다."}, status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['DELETE','PUT'])
+@permission_classes([IsAuthenticated])
+def comment_update_or_delete(request, article_id, comment_id):
+  article = get_object_or_404(Article, id=article_id)
+  comment = get_object_or_404(Comment, id=comment_id)
+  if request.method == 'DELETE':
+    if request.user == comment.user:
+      comment.delete()
+      return Response({"status":"OK"})
+    else:
+      return Response({"status":"FAIL", "error_msg":"본인 댓글만 삭제할 수 있습니다."}, status=status.HTTP_403_FORBIDDEN)
+  else:
+    if request.user == comment.user:
+      serializer = CommentSerializer(comment, data=request.data)
+      if serializer.is_valid(raise_exception=True):
+        serializer.save(user=request.user, article=article)
+      # comment.content = request.data.get('content')
+      # comment.save()
+      # serializer = CommentSerializer(comment)
+      return Response({"status":"OK", "data":serializer.data})
+    else:
+      return Response({"status":"FAIL","error_msg":"본인 댓글만 수정할 수 있습니다."}, status=status.HTTP_403_FORBIDDEN)
